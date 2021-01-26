549f1d8b79f23aa90dba8ce20e5f398514043d5a
Vikram Sundar <Vikram.Sundar@castandcrew.com>
2021-01-25 16:09:46 -0800

    Hashmap, palindrome permuatation

---
+++ src/main/java/com/practice/java/hashmap/Palindrome.java
@@ -0,0 +1,39 @@
+package com.practice.java.hashmap;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author <a href="mailto:vikram.sundar@castandcrew.com">Vikram Sundar Subbiah</a>
+ */
+public class Palindrome {
+
+    public boolean isPalindrome(final String value) {
+        Objects.requireNonNull(value);
+        String convertedString = value.toLowerCase();
+        int i = 0;
+        int j = convertedString.length() - 1;
+        while (i < j) {
+            if (convertedString.charAt(i) != convertedString.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean canFormPalindrome(final String value) {
+        Objects.requireNonNull(value);
+        Set<Character> characterSet = new HashSet<>();
+        for (Character ch : value.toCharArray()) {
+            if (characterSet.contains(ch)) {
+                characterSet.remove(ch);
+            } else {
+                characterSet.add(ch);
+            }
+        }
+        return characterSet.size() <= 1;
+    }
+}
---
+++ src/test/java/com/practice/java/hashmap/PalindromeTest.java
@@ -0,0 +1,30 @@
+package com.practice.java.hashmap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author <a href="mailto:vikram.sundar@castandcrew.com">Vikram Sundar Subbiah</a>
+ */
+class PalindromeTest {
+
+    private Palindrome palindrome;
+
+    @BeforeEach
+    void setUp() {
+        palindrome = new Palindrome();
+    }
+
+    @Test
+    void isPalindrome() {
+        assertEquals(true, palindrome.isPalindrome("iTopiNonAvevanoNipoti"));
+        assertEquals(false, palindrome.isPalindrome("iGattiNonAvevanoCugini"));
+    }
+
+    @Test
+    void canFormPalindrome() {
+        assertEquals(true, palindrome.canFormPalindrome("edified"));
+    }
+}
