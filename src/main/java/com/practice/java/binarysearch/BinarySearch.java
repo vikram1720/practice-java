6610ecd9243dc995123225aeab9a7ae70e6b2b67
Vikram Sundar <Vikram.Sundar@castandcrew.com>
2021-01-27 13:43:06 -0800

    binary search additional scenarios

--- src/main/java/com/practice/java/binarysearch/BinarySearch.java
+++ src/main/java/com/practice/java/binarysearch/BinarySearch.java
@@ -1,10 +1,13 @@
 package com.practice.java.binarysearch;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
+@Slf4j
 public class BinarySearch {
 
-    public int binarySearchIndex(int key, List<Integer> values) {
+    public int search(int key, List<Integer> values) {
         int low = 0;
         int high = values.size() - 1;
         while (low <= high) {
@@ -20,7 +23,7 @@ public class BinarySearch {
         return -1;
     }
 
-    public int binarySearchFirstOfK(int key, List<Integer> values) {
+    public int searchFirstOccurrenceOfKey(int key, List<Integer> values) {
         int result = -1;
         int low = 0;
         int high = values.size() - 1;
@@ -32,6 +35,22 @@ public class BinarySearch {
                 result = mid;
                 high = mid - 1;
             } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public int firstElementGreaterThanKeyIndex(int key, List<Integer> values) {
+        int result = -1;
+        int low = 0;
+        int high = values.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (values.get(mid) <= key) {
+                low = mid + 1;
+            } else {
+                result = mid;
                 high = mid - 1;
             }
         }
--- src/test/java/com/practice/java/binarysearch/BinarySearchTest.java
+++ src/test/java/com/practice/java/binarysearch/BinarySearchTest.java
@@ -16,19 +16,27 @@ class BinarySearchTest {
     @BeforeEach
     void setUp() {
         binarySearch = new BinarySearch();
-        values = List.of(-14, -10, 2, 108, 108, 243, 285, 285, 401);
+        values = List.of(-14, -10, 2, 108, 108, 243, 285, 285, 285, 401);
     }
 
     @Test
     void binarySearchIndexTest() {
-        assertEquals(5, binarySearch.binarySearchIndex(243, values));
-        assertEquals(-1, binarySearch.binarySearchIndex(400, values));
-        assertEquals(2, binarySearch.binarySearchIndex(2, values));
+        assertEquals(5, binarySearch.search(243, values));
+        assertEquals(-1, binarySearch.search(400, values));
+        assertEquals(2, binarySearch.search(2, values));
     }
 
     @Test
     void binarySearchFirstOfKTest() {
-        assertEquals(3, binarySearch.binarySearchFirstOfK(108, values));
-        assertEquals(6, binarySearch.binarySearchFirstOfK(285, values));
+        assertEquals(3, binarySearch.searchFirstOccurrenceOfKey(108, values));
+        assertEquals(6, binarySearch.searchFirstOccurrenceOfKey(285, values));
+    }
+
+    @Test
+    void firstElementGreaterThanKey() {
+        assertEquals(1, binarySearch.firstElementGreaterThanKeyIndex(-13, values));
+        assertEquals(9, binarySearch.firstElementGreaterThanKeyIndex(285, values));
+        assertEquals(-1, binarySearch.firstElementGreaterThanKeyIndex(500, values));
+        assertEquals(5, binarySearch.firstElementGreaterThanKeyIndex(108, values));
     }
 }
